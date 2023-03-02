b166f83cfea706f0b47b44ea1cd7a470f2981ab5
Konstantin Barkalov <barkalov@vmk.unn.ru>
2023-03-02 14:32:44 +0300

    simple objective function

--- src/directPM.cpp
+++ src/directPM.cpp
@@ -15,10 +15,10 @@ int trialCount = 0;
 bool isFindOptim = false;
 int iter = 0;
 double** points = 0;
-/// Точность с которой ищем решение
-double methodAccuracy = 0.01;
 
-double objective(int N, const double *x, int *undefinedFlag, void *data) 
+/*
+// Целевая функция для DIRECT, в которой значения функции вычисляются через dll
+double objective(int N, const double *x, int *undefinedFlag, void *data)
 {
   if (points != 0)
   {
@@ -73,124 +73,73 @@ double objective(int N, const double *x, int *undefinedFlag, void *data)
   //return genopt_evaluate(x);
   return sum;
 }
+*/
+
+
+//Простая одномерная целевая функция для DIRECT, в которой реализовано вычисление значения функции
+double simple_objective(int N, const double* x, int* undefinedFlag, void* data)
+{
+    trialCount++;//счетчик числа испытаний - реализован как глобальная переменная
+    return x[0] * x[0] - cos(18 * x[0]);
+}
 
 int main(int argc, char **argv)
 {
-
-  TProblemManager manager;
-  if (argc > 1)
-  {
-    std::string problemName = argv[1];
-    std::string problepConf = "";
-    if (argc > 2)
-    {
-      problepConf = argv[2]; 
-    }
-
-    int err;
-    if (manager.LoadProblemLibrary(problemName) != TProblemManager::OK_)
-      return 0;
-    IProblem* problem = manager.GetProblem();
-
-
-    err = problem->SetDimension(2);
-    err = problem->SetConfigPath(problepConf);
-    err = problem->Initialize();
-    
-    int dimension = problem->GetDimension();
-    double* lower_bounds = new double[dimension];//[2] = { -1, -1 };
-    double* upper_bounds = new double[dimension];// [2] = { 1, 1 };
-    problem->GetBounds(lower_bounds, upper_bounds);
+    int dimension = 1;  //размерность
+    double* lower_bounds = new double[dimension]; 
+    double* upper_bounds = new double[dimension];
+    lower_bounds[0] = -2.5;//нижняя границе интервала поиска
+    upper_bounds[0] =  1.5;//верхняя граница интервала поиска
 
     //Возвращаемое значение
-    double* minx = new double[dimension];// [2];
+    double* minx = new double[dimension];
     double minf = -std::numeric_limits<double>::infinity(); // include <limits>
     //Параметры
-    int max_feval = 10000; // Maximum number of function evaluation
-    int max_iter = 10000; // Maximum number of algorithm iterations (different from max);
-    double magic_eps     = 0.0001;
+    int max_feval = 1000; // Maximum number of function evaluation
+    int max_iter = 1000; // Maximum number of algorithm iterations (different from max);
+    double magic_eps     = 0.0001; //Параметр eps метода DIRECT
     double magic_eps_abs = 0.0001;
-    double volume_reltol = methodAccuracy / sqrt(dimension); /// 100.0; // объем!!!// поделить на корень из N
-    double sigma_reltol  = 0.00000;//9; // 
+    double volume_reltol = 0.001; //Точность поиска
+    double sigma_reltol  = 0.00000;
     int force_stop = false;
 
-    if (argc > 3)
-    {
-      points = new double* [max_feval * 2];
-    }
 
-    //Запуск
+    //Запуск метода; результаты работы выводятся в файл
     FILE* f = fopen("outDirect.txt", "w");
     direct_return_code retcode = direct_optimize(
-      objective, 
-      problem,
-      dimension,
-      lower_bounds, 
-      upper_bounds,
-      minx,
-      &minf,
-      max_feval, 
-      max_iter,
-      0.0, 0.0,
-      magic_eps, 
-      magic_eps_abs,
-      volume_reltol, 
-      sigma_reltol,
-      &force_stop,
-      DIRECT_UNKNOWN_FGLOBAL,
-      DIRECT_UNKNOWN_FGLOBAL,
-      f,
-      DIRECT_ORIGINAL);
+        simple_objective,
+        NULL,
+        dimension,
+        lower_bounds, 
+        upper_bounds,
+        minx,
+        &minf,
+        max_feval, 
+        max_iter,  
+        0.0, 0.0,  //ограничение по времени работы?
+        magic_eps, 
+        magic_eps_abs,
+        volume_reltol, 
+        sigma_reltol,
+        &force_stop,
+        DIRECT_UNKNOWN_FGLOBAL,
+        DIRECT_UNKNOWN_FGLOBAL,
+        f,
+        DIRECT_ORIGINAL);
     fclose(f);
     //printf("min f = %g at (%g,%g) after %d evals, return value %d\n",
     //              minf, x[0], x[1], cnt, info);
 
     for (int i = 0; i < dimension; i++)
-      printf("x[%d] = %lf\n", i, minx[i]);
+    {
+        printf("x[%d] = %lf\n", i, minx[i]);
+    }
     printf("min = %lf\n", minf);
     printf("retcode = %d\n", retcode);
     printf("Point = %d\n", trialCount);
     if (((retcode == 1) || (retcode == 4) || (retcode == 5)) && (isFindOptim))
     {
-      printf("Global optimum FOUND!\n");
+        printf("Global optimum FOUND!\n");
     }
-
-    if (argc > 3)
-    {
-      printf("Iteration = %d\n", iter);
-      std::string pointLogName = argv[3];
-      FILE* pointLog = fopen(pointLogName.c_str(), "w");
-
-      fprintf(pointLog, "%d\n", trialCount);
-      for (int i = 0; i < trialCount; i++)
-      {
-        for (int j = 0; j < dimension; j++)
-          fprintf(pointLog, "%lf ", points[i][j]);
-        fprintf(pointLog, "| %lf\n", points[i][dimension]);
-      }
-
-      //for (int j = 0; j < dimension; j++)
-      //  fprintf(pointLog, "%lf ", minx[j]);
-      //fprintf(pointLog, "\n");
-
-      for (int j = 0; j < dimension; j++)
-        fprintf(pointLog, "%lf ", points[trialCount - 1][j]);
-      fprintf(pointLog, "| %lf\n", points[trialCount - 1][dimension]);
-
-      double* BestTrialy = new double [dimension];
-      int err = problem->GetOptimumPoint(BestTrialy);
-      double optVal = 0;
-      problem->GetOptimumValue(optVal);
-
-      for (int j = 0; j < dimension; j++)
-        fprintf(pointLog, "%lf ", BestTrialy[j]);
-      fprintf(pointLog, "| %lf", optVal);
-
-      fclose(pointLog);
-      delete [] points;
-    }
-  }
-  //printf("dif = %lf\n", fabs(minf -  genopt_get_global_minimum()));
-  //genopt_terminate();
   return 0;
 }
